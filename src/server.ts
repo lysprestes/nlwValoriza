6022fb540b8267ab38271f8dd090b5f0401c42a5
Lys <lysprestes@gmail.com>
2022-01-24 20:07:29 -0500

    Creating DB, Routes, Controllers and Services

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import { router } from './routes';
+
+import './database';
 
 // @types/express
 const app = express();
 
-app.post('/test-post', (req, res) => {
-  return res.send('Hello World!');
-});
+app.use(express.json());
 
-app.listen(3000, () => console.log('Server is running'));
+app.use(router);
+
+app.listen(3000, () => console.log('---> ( ͡° ͜ʖ ͡°).｡.:*♡ Server is running! .｡.:*♡ <---'));
